66c59921bc631d86c72a51ada6da2a426bca48fd
Carbon-42 <116390874+Carbon-42@users.noreply.github.com>
2023-10-24 19:06:54 -0600

    debug false

--- recipeapp/settings.py
+++ recipeapp/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = os.environ.get(
     'DJANGO_SECRET_KEY', 'django-insecure-ml49cp(e)=yakpevh4xz)3w)6xuq6kv7g&3^xf^)gr-n3&p#%9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'salty-headland-07976-f1493a7cebd8.herokuapp.com', '127.0.0.1', 'localhost', '0.0.0.0']
